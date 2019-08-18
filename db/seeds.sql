e930354ed2805fa59198bf4a03265eecdfac0e46
Backslash <lanceadrian13@gmail.com>
2019-08-18 16:05:29 -0400

    organized/ created necessary files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("bacon egg and cheese");
+INSERT INTO burgers (burger_name) VALUES ("veggie burger");
